054f064081c2aa6f90db69373fffa53478029f05
RADM90 <joel.jwpark@gmail.com>
2023-01-26 19:23:45 +0900

    fix: Changing Ports for deploying on AI Stages Server

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ run_server:
 	python -m gpu_dummy
 
 run_client:
-	python -m streamlit run app/main.py
+	python -m streamlit run app/main.py --server.port=30001 --server.fileWatcherType none
 
 run_app:	run_client	run_server
--- gpu_dummy/__main__.py
+++ gpu_dummy/__main__.py
@@ -1,3 +1,3 @@
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("gpu_dummy.main:app", host="0.0.0.0", port=8002, workers=4, reload=True)
+    uvicorn.run("gpu_dummy.main:app", host="0.0.0.0", port=30002, workers=4, reload=True)
