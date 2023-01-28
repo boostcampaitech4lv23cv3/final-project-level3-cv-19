56284c8874f5a465d20166d3b3f3ef9c666f6c3c
ths3847 <ths3847@gmail.com>
2023-01-28 06:29:50 +0000

    add: yolov7 model

---
+++ yolov7/custom_detect.py
@@ -0,0 +1,95 @@
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
+def detect(source="test.mp4"):
+    save_img = True
+    weights = ['best_30e.pt']
+    conf_thres = 0.5
+    iou_thres = 0.45
+    imgsz = 640
+    project = 'runs/detect'
+    name = 'exp'
+    save_dir = Path(increment_path(Path(project) / name, exist_ok=False))  # increment run
+    (save_dir).mkdir(parents=True, exist_ok=True)
+    
+    # Initialize
+    device = select_device('')
+    half = device.type != 'cpu'  # half precision only supported on CUDA
+
+    # Load model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    if half:
+        model.half()  # to FP16 
+    # Set Dataloader
+    vid_path, vid_writer = None, None
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
+    # Get names and colors
+    names = model.module.names if hasattr(model, 'module') else model.names
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+    # Run inference
+    if device.type != 'cpu':
+        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    for path, img, im0s, vid_cap in dataset:
+        img = torch.from_numpy(img).to(device)
+        img = img.half() if half else img.float()  # uint8 to fp16/32
+        img /= 255.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+        # Inference
+        with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
+            pred = model(img, augment=False)[0]
+        # Apply NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes=None, agnostic=False)
+        # Process detections
+        for i, det in enumerate(pred): 
+            p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            # gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+            # Save results (image with detections)
+  
+            if dataset.mode == 'image':
+                cv2.imwrite(save_path, im0)
+            else:  # 'video'
+                if vid_path != save_path:  # new video
+                    vid_path = save_path
+                    if isinstance(vid_writer, cv2.VideoWriter):
+                        vid_writer.release()  # release previous video writer
+                    fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                    w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                    h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                vid_writer.write(im0)
+if __name__ == '__main__':
+    with torch.no_grad():
+        detect()
