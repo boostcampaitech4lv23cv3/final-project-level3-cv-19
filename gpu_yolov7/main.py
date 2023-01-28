c2a923e27ec463f5beb9fc716532c38d3c23e8d6
ths3847 <ths3847@gmail.com>
2023-01-28 16:27:59 +0000

    add: load model added

--- gpu_yolov7/main.py
+++ gpu_yolov7/main.py
@@ -11,21 +11,29 @@ from fastapi import FastAPI, UploadFile, File, Response, Request
 import shlex
 from subprocess import check_call
 import time
+from gpu_yolov7.detect import detect
+import os
+import sys
+yolo_path = '/'.join(os.path.dirname(os.path.realpath(__file__)).split('/')[:-1])+'/yolov7'
+sys.path.append(yolo_path)
+from models.experimental import attempt_load
+from utils.torch_utils import select_device
 
 app = FastAPI()
 
-@app.post("/results")
+#load model
+weights = ['gpu_yolov7/best_30e.pt']
+device = select_device('')
+model = attempt_load(weights, map_location=device)  # load FP32 model
+
+@app.post("/")
 async def predict(request: Request, files: List[UploadFile] = File(...)):
     for file in files:
-        # head = dict(request.headers)
-        # file_name = head["filename"]
         file_name = file.filename
-        # user_id = head["user_id"]
+        user_id = request.headers.get('user_id')
         video_bytes = await file.read()
         result = None
-        with torch.no_grad():
-            # detect(source='gpu_dummy/input/test.mp4')
-            with open(file_name, 'wb') as f:
-                f.write(video_bytes)
-            result = detect(source=file_name)
-        return Response(content=result, media_type="video/mp4", headers={"filename": file_name})
+        with open(file_name, 'wb') as f:
+            f.write(video_bytes)
+        result = detect(source=file_name, model=model)
+        return Response(content=result, media_type="video/mp4", headers={"user_id": user_id,"filename": file_name})
