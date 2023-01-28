7d8a4426063b9ca62fb00411070dca0350573089
hadam <bhd2024@gmail.com>
2023-01-28 18:58:20 +0900

    Add: Response headers user_id

--- gpu_server.py
+++ gpu_server.py
@@ -11,6 +11,7 @@ async def rsp(request: Request, files: List[UploadFile] = File(...)):
         # file_name = request.headers["filename"]
         # user_id = requests.headers["user_id"]
         file_name = file.filename
+        user_id = request.headers.get('user_id')
         video_bytes = await file.read()
         with open(file_name, 'wb') as f:
             f.write(video_bytes)
@@ -18,7 +19,7 @@ async def rsp(request: Request, files: List[UploadFile] = File(...)):
         result_b = None
         with open(result, 'rb') as f:
             result_b = f.read()
-        return Response(content=result_b, media_type="video/mp4", headers={"filename": file_name})
+        return Response(content=result_b, media_type="video/mp4", headers={"user_id": user_id,"filename": file_name})
 
 
 if __name__ == "__main__":
