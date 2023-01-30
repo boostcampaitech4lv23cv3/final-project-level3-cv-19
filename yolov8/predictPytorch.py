ce03b2d1a5285503651ce1d0487ac0850390fdfc
RADM90 <joel.jwpark@gmail.com>
2023-01-30 19:14:33 +0900

    fix: Dir Sturcture for Merging Branch

--- yolov8/predictPytorch.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8s.pt")  # load a pretrained model (recommended for training)
-model.info()
-
-# Use the model
-results = model("https://ultralytics.com/images/bus.jpg", verbose=True, save=True)  # predict on an image
-
