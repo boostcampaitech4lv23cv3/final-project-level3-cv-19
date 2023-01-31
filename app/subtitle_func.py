1b58edfd71e82471ca37cc5222be1f84587ecaa4
RADM90 <joel.jwpark@gmail.com>
2023-01-31 22:42:57 +0900

    fix: HTML page string editted for iFrame test

--- app/subtitle_func.py
+++ app/subtitle_func.py
@@ -1,7 +1,9 @@
 import pathlib
 import os
+from app.main import dir_func
 
-ROOT_PATH = pathlib.Path.root
+PRJ_ROOT_PATH = pathlib.Path(__file__).parent.parent.absolute()
+APP_PATH = os.path.join(PRJ_ROOT_PATH, "app")
 
 
 def second_to_timecode(x: float) -> str:
@@ -12,7 +14,7 @@ def second_to_timecode(x: float) -> str:
     return f"{hour:.2d}:{minute:.2d}:{second:.2d},{millisecond:.3d}"
 
 
-def json2srt(src: dict, fps=10, dst=None, save: bool = False):
+def json2srt(src: dict, fps=10, session_id=None, dst_fn="subtitle", save: bool = False):
     frame_time_in_sec = 1/fps
     ts_start = .0
     ts_end = ts_start + frame_time_in_sec
@@ -30,16 +32,40 @@ def json2srt(src: dict, fps=10, dst=None, save: bool = False):
 
     parsed_str = "\n".join(subtitle_arr)
     if save:
-        with open(dst, 'w+', encoding='utf-8') as f:
+        save_path = os.path.join(APP_PATH, "srt", session_id, dst_fn)
+        dir_func(save_path, rmtree=False, mkdir=True)
+        with open(save_path, 'w+', encoding='utf-8') as f:
             f.writelines(parsed_str)
     return parsed_str
 
 
-def get_html_with_subvideo(session_id: str, sub_input=None) -> str:
-    if sub_input is not None:
-        srt = json2srt(sub_input)
-    else:
-        srt = open(os.path.join(ROOT_PATH, "srt", session_id, "subtitle.srt"), encoding="utf-8").read()
-
-    html_path = os.path.join(ROOT_PATH, "html", session_id, "subtitle.html")
-    return html_path
+def get_html(session_id: str, srt_fname: str = "subtitle", sub_json_input=None) -> str:
+    srt_path = os.path.join(APP_PATH, "srt", session_id, srt_fname+".srt")
+    video_path = os.path.join(APP_PATH, "result", session_id)
+    if sub_json_input is not None:
+        with open(srt_path, 'w+', encoding='utf-8') as f:
+            f.writelines(json2srt(sub_json_input))
+    html_path = os.path.join(APP_PATH, "html", session_id)
+    dir_func(html_path, rmtree=False, mkdir=True)
+    html_str = f"""
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/video.js@7.9.3/dist/video-js.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/video.js@7.9.3/dist/video.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/videojs-sublime-skin@3.3.2/dist/videojs-sublime-skin.min.css"></script>
+</head>
+<body>
+  <video id="my-video" class="video-js vjs-sublime-skin" controls preload="auto" width="640">
+    <source src="{os.path.join(video_path, os.listdir(video_path)[0])}" type='video/mp4'>
+    <track src="{srt_path}" kind="subtitles" srclang="ko" label="한국어">
+  </video>
+  <script>
+    var player = videojs('my-video');
+  </script>
+</body>
+</html>
+    """
+    with open(html_path+"subs.html", 'w+', encoding='utf-8') as f:
+        f.writelines(html_str)
+    return html_path+"subs.html"
