6c6d69798f5155eb36e702c87c51bfc3b242590a
hadam <bhd2024@gmail.com>
2023-02-01 17:19:14 +0900

    add: new filtering added

--- gpu_server.py
+++ gpu_server.py
@@ -19,9 +19,10 @@ async def rsp(request: Request, files: List[UploadFile] = File(...)):
         result_b = None
         with open(result, 'rb') as f:
             result_b = f.read()
-        #with open(result_json,'rb') as f:
+        #with open(result_json,'r') as f:
         #    result_json_b = f.read()
         return Response(content=result_b, media_type="video/mp4", headers={"user_id": user_id,"filename": file_name})
+        #return Response(content=result_b, data=result_json, media_type="video/mp4", headers={"user_id": user_id,"filename": file_name})
 
 
 if __name__ == "__main__":
