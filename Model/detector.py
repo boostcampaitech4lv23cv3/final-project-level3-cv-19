460f77d0e6b3a213dad102b4d02111fd54cb4ec6
RADM90 <joel.jwpark@gmail.com>
2023-02-03 22:13:46 +0900

    fix: Refectoring  with Removing duplicated Encoding process

--- Model/detector.py
+++ Model/detector.py
@@ -2,10 +2,9 @@ import os
 import cv2
 import torch
 import json
-import time
 import math
 from pathlib import Path
-from app.ffmpeg_func import h264_encoding
+from app.utils import dir_func
 
 from ultralytics.nn.autobackend import AutoBackend
 from ultralytics.yolo.data.dataloaders.stream_loaders import LoadImages
@@ -20,106 +19,65 @@ APP_PATH = os.path.join(PRJ_ROOT_PATH, "app")
 SAVE_PATH = os.path.join(MODEL_DIR, "save")
 
 
-def detect(src: str, session_id: str, dst: str, conf_thres=0.25):
-    start = time.time()
+def detect(src: str, session_id: str, conf_thres=0.25, THRESHOLD_y=0.7):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print('Inference by', device)
-    model = AutoBackend(weights=os.path.join(MODEL_DIR, "yolov8", 'yolov8n_custom.pt'), device=device, dnn=False, fp16=False)
+    print('Inference with', device)
 
-    model.eval()
-    stride, names, pt = model.stride, model.names, model.pt
-    imgsz = (640, 640)
-    imgsz = check_imgsz(imgsz, stride=stride)
-
-    dataset = LoadImages(src, imgsz=imgsz, stride=stride, auto=pt, vid_stride=1)
-    frames = []
-    data = {}
-
-    T1 = 0.7  # threshold1 y
-    T2_x1 = 0.3  # threshold2 xleft
-    T2_x2 = 0.7  # threshold2 xright
-    T2_y = 0.9  # threshold2 y
-
-    time_record1 = time.time()
-    # Local Variables Assignment
     tmp_path = os.path.join(APP_PATH, "tmp", session_id)
-    json_file_path = None
-    p = None
-    fps = None
-    w, h = None, None
-    time_record2 = None
+    img_dst = os.path.join(tmp_path, "img_dir")
+    TXT_FILE = os.path.join(tmp_path, f'dist_degree.txt')
+    dir_func(img_dst, rmtree=False, mkdir=True)
+
+    model = AutoBackend(weights=os.path.join(MODEL_DIR, "yolov8", 'yolov8n_custom.pt'), device=device, dnn=False, fp16=False)
+    model.eval()
+
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_imgsz((640, 640), stride=stride)
+    dataset = LoadImages(src, imgsz=imgsz, stride=stride, auto=pt, vid_stride=1)
+
+    json_obj = {}
 
     for frame_idx, batch in enumerate(dataset, 1):
-        path, im, im0s, vid_cap, s = batch
-        annotator = None
-        im = torch.from_numpy(im)
+        _, img_nparr, im0s, vid_cap, s = batch
+        annotator = Annotator(im0s, line_width=2, example=str(names))
+        img_nparr = torch.from_numpy(img_nparr)
 
-        im = im.float()
-        im /= 255.0
-        if len(im.shape) == 3:
-            im = im[None]
+        img_nparr = img_nparr.float()
+        img_nparr /= 255.0
+        if len(img_nparr.shape) == 3:
+            img_nparr = img_nparr[None]
 
-        im = im.to(device)
-        preds = model(im)
-        preds = non_max_suppression(preds, conf_thres=conf_thres, iou_thres=0.45, classes=None, agnostic=False, max_det=1000)
+        img_nparr = img_nparr.to(device)
+        bboxes_data = non_max_suppression(model(img_nparr), conf_thres=conf_thres, iou_thres=0.45, classes=None, agnostic=False, max_det=1000)
 
-        results = []
-
-        for i, pred in enumerate(preds):
-            p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
-            p = Path(p)
-            json_file_path = os.path.join(tmp_path, f'{p.stem}.json')
-
+        for i, bbox in enumerate(bboxes_data):
             shape = im0s[i].shape if isinstance(im0s, list) else im0s.shape
-            pred[:, :4] = scale_boxes(im.shape[2:], pred[:, :4], shape).round()
-            results.append(Results(boxes=pred, orig_shape=shape[:2]))
-            annotator = Annotator(im0, line_width=2, example=str(names))
-            det = results[i].boxes
-            i = 0
-            data[f'{frame_idx:04d}'] = {}
-            TXT_FILE = os.path.join(tmp_path, f'dist_degree_{p.stem}.txt')
+            img_h, img_w, _ = shape
+            bbox[:, :4] = scale_boxes(img_nparr.shape[2:], bbox[:, :4], (img_h, img_w)).round()
 
-            with open(TXT_FILE, 'a') as mathfile:
-                mathfile.write(f'{frame_idx:04d}:\n')
-            for d in reversed(det):
-                box = d.xyxy.squeeze().tolist()
-                if box[3] > shape[0] * T1:
-                    warn = 2
-                    d_x, d_y = (box[0] + box[2]) / 2 - shape[1] / 2, box[3] - shape[0]
-                    dist = math.sqrt(pow(d_x, 2) + pow(d_y, 2))
-                    angle = 90 - math.atan2(-d_y, d_x) * 180 / math.pi
-                    with open(TXT_FILE, 'a') as mathfile:
-                        mathfile.write(f'{i} {dist:.1f} {angle:.1f} {d.xyxy.squeeze().tolist()}\n')
-                    if dist < shape[0] * 0.1 or (
-                            shape[0] * 0.1 < dist < shape[0] * 0.2 and -45 <= angle <= 45) or -15 <= angle <= 15:
-                        warn = 1
+            json_obj[f'{frame_idx:04d}'] = {}
+            with open(TXT_FILE, 'a') as f:
+                f.write(f'{frame_idx:04d}:\n')
+                for obj_id, obj in enumerate(reversed(Results(boxes=bbox, orig_shape=(img_h, img_w)).boxes), 1):
+                    x_min, y_min, x_max, y_max = obj.xyxy.squeeze().tolist()
+                    if y_max > img_h * THRESHOLD_y:
+                        warn = 2
+                        d_x = (x_min + x_max) / 2 - img_w / 2
+                        d_y = y_max - img_h
+                        dist = math.sqrt(pow(d_x, 2) + pow(d_y, 2))
+                        angle = 90 - math.atan2(-d_y, d_x) * 180 / math.pi
 
-                    cls, conf = d.cls.squeeze(), d.conf.squeeze()
-                    c = int(cls)
-                    label = f'{model.names[c]}{conf:.2f}'
-                    annotator.box_label(d.xyxy.squeeze(), label, color=colors(4 * (warn - 1), True))
-                    data[f'{frame_idx:04d}'][f'{i}'] = {"class": f'{model.names[c]}', "warning_lv": f'{warn}', "location": f'{int(((box[0] + box[2]) // 2) // (shape[1] // 3))}'}
-                    i += 1
+                        f.write(f'{obj_id:02d} {dist:.1f} {angle:.1f} {obj.xyxy.squeeze().tolist()}\n')
+                        if dist < img_h * 0.1 or (img_h * 0.1 < dist < img_h * 0.2 and -45 <= angle <= 45) or -15 <= angle <= 15:
+                            warn = 1
 
-        im0 = annotator.result()
-        frames.append(im0)
-        time_record2 = time.time()
+                        cls, conf = obj.cls.squeeze(), obj.conf.squeeze()
+                        c = int(cls)
+                        label = f'{model.names[c]}{conf:.2f}'
+                        annotator.box_label(obj.xyxy.squeeze(), label, color=colors(4 * (warn - 1), True))
+                        json_obj[f'{frame_idx:04d}'][f'{obj_id:02d}'] = {"class": f'{model.names[c]}', "warning_lv": f'{warn}', "location": f'{int(((x_min + x_max) // 2) // (img_w // 3))}'}
+                        obj_id += 1
 
-        fps = int(vid_cap.get(cv2.CAP_PROP_FPS))
-        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        cv2.imwrite(os.path.join(img_dst, f"{frame_idx:04}.jpg"), annotator.result())
 
-    with open(json_file_path, 'w') as file:
-        json.dump(data, file, indent=4)
-    json_obj = json.dumps(data, ensure_ascii=False, indent=None, sort_keys=True)
-
-    vid_writer = cv2.VideoWriter(str(p.name), cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-    for i in range(len(frames)):
-        vid_writer.write(frames[i])
-    vid_writer.release()
-
-    h264_encoding(str(p.name), dst)
-
-    with open(os.path.join(tmp_path, f'{p.stem}.txt'), 'w') as file:
-        file.write(f'model set: {time_record1 - start:.4f} sec, inference:{time_record2 - time_record1:.4f} sec, write & encoding:{time.time() - time_record2:.4f} sec\n')
-    return dst, json_obj
+    return json.dumps(json_obj, ensure_ascii=False, indent=None, sort_keys=True)
--- app/main.py
+++ app/main.py
@@ -14,15 +14,6 @@ from Model.detector import detect
 
 from requests import get
 
-# if st.session_state.get("a", False):
-#     st.session_state.disabled = True
-# elif st.session_state.get("a", True):
-#     st.session_state.disabled = False
-
-# def disable(b, c):
-#     st.session_state["disabled"] = b
-#     st.session_state["disabled"] = c
-
 
 def get_session_id() -> str:
     ctx = get_script_run_ctx()
@@ -41,7 +32,6 @@ subtitle_ext = "vtt"
 # PATH SETTINGS
 upload_path = f"app/uploaded/{user_session}/"
 dst_path = f"app/result/{user_session}/"
-html_path = f"app/html/{user_session}/"
 tmp_path = f"app/tmp/{user_session}/"
 wav_path = f"app/audio/{user_session}/"
 
@@ -50,7 +40,6 @@ def main():
     st.title("보행 시 장애물 안내 서비스")
     st.write(f"Session ID : {user_session}")
 
-    # uploaded_file = st.file_uploader("동영상을 선택하세요", type=["mp4"], key="c", disabled=st.session_state.get("disabled", True))
     placeholder = st.empty()
     upload_place = st.empty()
     uploaded_file = upload_place.file_uploader("동영상을 선택하세요", type=["mp4"])
@@ -66,12 +55,9 @@ def main():
             upload_place.empty()
         dir_func(tmp_path, rmtree=True, mkdir=True)
         preprocessed_file = os.path.join(tmp_path, "preprocessed.mp4")
-        result_video_file = os.path.join(dst_path, "resultvideo.mp4")
         result_av_file = os.path.join(dst_path, "result.mp4")
 
         col_slide, col_button = st.columns([2, 1])
-        # slide_value = col_slide.slider("Confidence Lv Threshold", min_value=0.1, max_value=1.0, value=0.25, step=0.05, key="b", disabled=st.session_state.get("disabled", True))
-        # button_value = col_button.button("Start Process", key="a", on_click=disable, args=(True, True))
         slide_value = col_slide.slider("Confidence Lv Threshold", min_value=0.1, max_value=1.0, value=0.25, step=0.05)
         button_value = col_button.button("Start Process")
 
@@ -81,7 +67,9 @@ def main():
                 placeholder.success("동영상 전처리 완료")
                 with st.spinner("객체 탐지 중..."):
                     if 1:  # Pytorch
-                        result_video_file, frame_json = detect(preprocessed_file, user_session, result_video_file, conf_thres=slide_value)
+                        img_dir = os.path.join(tmp_path, "img_dir")
+                        dir_func(img_dir, rmtree=True, mkdir=True)
+                        frame_json = detect(preprocessed_file, user_session, conf_thres=slide_value)
                     else:  # TensorRT
                         from Model.onnx_tensorrt.utils import BaseEngine
                         pred = BaseEngine(engine_path='./Model/onnx_tensorrt/yolov8n_custom.trt')
@@ -90,7 +78,7 @@ def main():
                 json2sub(session_id=user_session, json_str=frame_json, fps=TARGET_FPS, save=True)
                 json2audio(dst_path=wav_path, json_str=frame_json, fps=TARGET_FPS, save=True)
                 audio_file = os.path.join(wav_path, "synthesized_audio.wav")
-                combine_video_audio(result_video_file, audio_file, result_av_file)
+                combine_video_audio(img_dir, audio_file, result_av_file, fps=TARGET_FPS)
                 components.html(f"""
                   <div class="container">
                     <video controls preload="auto" width="{container_w}" autoplay crossorigin="anonymous">
