e540478e2d66a5775f2349c8016796b3f7ee82a9
hadam <bhd2024@gmail.com>
2023-02-04 23:02:37 +0900

    add: warning visualize & filtering logic changed

--- Model/detector.py
+++ Model/detector.py
@@ -5,6 +5,7 @@ import json
 import math
 from pathlib import Path
 from app.utils import dir_func
+import numpy as np
 
 from ultralytics.nn.autobackend import AutoBackend
 from ultralytics.yolo.data.dataloaders.stream_loaders import LoadImages
@@ -35,11 +36,33 @@ def detect(src: str, session_id: str, conf_thres=0.25, THRESHOLD_y=0.7):
     imgsz = check_imgsz((640, 640), stride=stride)
     dataset = LoadImages(src, imgsz=imgsz, stride=stride, auto=pt, vid_stride=1)
 
+    for batch in dataset:
+        _,img_nparr, im0s, vid_cap, s = batch
+        mask_h,mask_w,_=im0s.shape
+        mask = np.zeros(im0s.shape,np.uint8)
+        mask_thres1 = np.zeros((mask_h,mask_w),np.uint8)
+        mask_thres2 = np.zeros((mask_h,mask_w),np.uint8)
+        break
+
     json_obj = {}
-    dist_T1=0.1
+    """dist_T1=0.1
     dist_T2=0.2
     angle_T1=45
-    angle_T2=15
+    angle_T2=15"""
+
+    #mask = np.zeros((720,1280,3),np.uint8)
+    cv2.ellipse(mask,(int(mask_w/2),mask_h),(int(mask_h*0.6),int(mask_h*0.3)),0,180,360,(0,255,255),-1)
+    cv2.ellipse(mask,(int(mask_w/2),mask_h),(int(mask_h*0.2),int(mask_h*0.1)),0,180,360,(0,0,255),-1)
+    cv2.ellipse(mask,(int(mask_w/2),mask_h),(int(mask_h*0.4),int(mask_h*0.2)),0,240,300,(0,0,255),-1)
+    cv2.ellipse(mask,(int(mask_w/2),mask_h),(int(mask_h*0.6),int(mask_h*0.3)),0,255,285,(0,0,255),-1)
+
+    #mask_thres1
+    cv2.ellipse(mask_thres1,(int(mask_w/2),mask_h),(int(mask_h*0.6),int(mask_h*0.3)),0,180,360,255,-1)
+    #mask_thres2
+    cv2.ellipse(mask_thres2,(int(mask_w/2),mask_h),(int(mask_h*0.2),int(mask_h*0.1)),0,180,360,255,-1)
+    cv2.ellipse(mask_thres2,(int(mask_w/2),mask_h),(int(mask_h*0.4),int(mask_h*0.2)),0,240,300,255,-1)
+    cv2.ellipse(mask_thres2,(int(mask_w/2),mask_h),(int(mask_h*0.6),int(mask_h*0.3)),0,255,285,255,-1)
+
 
     for frame_idx, batch in enumerate(dataset, 1):
         _, img_nparr, im0s, vid_cap, s = batch
@@ -65,7 +88,7 @@ def detect(src: str, session_id: str, conf_thres=0.25, THRESHOLD_y=0.7):
                 f.write(f'{frame_idx:04d}:\n')
                 for obj_id, obj in enumerate(reversed(Results(boxes=bbox, orig_shape=(img_h, img_w)).boxes), 1):
                     x_min, y_min, x_max, y_max = obj.xyxy.squeeze().tolist()
-                    if y_max > img_h * THRESHOLD_y:
+                    """if y_max > img_h * THRESHOLD_y:
                         warn = 2
                         d_x = (x_min + x_max) / 2 - img_w / 2
                         d_y = y_max - img_h
@@ -83,10 +106,25 @@ def detect(src: str, session_id: str, conf_thres=0.25, THRESHOLD_y=0.7):
                         c = int(cls)
                         label = f'{model.names[c]}'
                         annotator.box_label(obj.xyxy.squeeze(), label, color=colors(4 * (warn - 1), True))
-                        json_obj[f'{frame_idx:04d}'][f'{obj_id:02d}'] = {"class": f'{model.names[c]}', "warning_lv": f'{warn}', "location": f'{int((((x_min + x_max)/ 2 - (img_w / 2)) /(img_h * 0.1)+3 )// 2)}'}
+                        json_obj[f'{frame_idx:04d}'][f'{obj_id:02d}'] = {"class": f'{model.names[c]}', "warning_lv": f'{warn}', "location": f'{int((((x_min + x_max)/ 2 - (img_w / 2)) /(img_h * 0.1)+3 )// 2)}'}"""
+                    
+                    d_x = (x_min + x_max) / 2 - img_w / 2
+                    d_y = y_max - img_h
+                    dist = math.sqrt(pow(d_x, 2) + pow(d_y, 2))
+                    angle = 90 - math.atan2(-d_y, d_x) * 180 / math.pi
+                    f.write(f'{obj_id:02d} {dist:.1f} {angle:.1f} {obj.xyxy.squeeze().tolist()}\n')
+                    warn=3
+                    
+                    if np.any((mask_thres1[int(y_min):int(y_max),int(x_min):int(x_max)] & np.ones((int(y_max-y_min),int(x_max-x_min)),np.uint8)) > 0):
+                        warn = 2
+                        if np.any((mask_thres2[int(y_min):int(y_max),int(x_min):int(x_max)] & np.ones((int(y_max-y_min),int(x_max-x_min)),np.uint8)) > 0):
+                            warn = 1
+                    cls = obj.cls.squeeze()
+                    c = int(cls)
+                    label = f'{model.names[c]}'
+                    annotator.box_label(obj.xyxy.squeeze(), label, color=colors(4 * (warn - 1), True))
+                    json_obj[f'{frame_idx:04d}'][f'{obj_id:02d}'] = {"class": f'{model.names[c]}', "warning_lv": f'{warn}', "location": f'{int((((x_min + x_max)/ 2 - (img_w / 2)) /(img_h * 0.1)+3 )// 2)}'}
                     
                     
-
-        cv2.imwrite(os.path.join(img_dst, f"{frame_idx:04}.jpg"), annotator.result())
-
+        cv2.imwrite(os.path.join(img_dst, f"{frame_idx:04}.jpg"), cv2.addWeighted(mask, 0.2, annotator.result(),0.8,0))
     return json.dumps(json_obj, ensure_ascii=False, indent=None, sort_keys=True)
